19fc6ae4bf3a7c1092e5e8d7ecfe90d9ff78c8de
ErwinP <codeempower00@gmail.com>
2024-07-13 01:50:14 -0500

    final4

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // Extiende directamente las configuraciones que necesitas
+  extends: [
+    'next/core-web-vitals',
+    'standard',
+    'plugin:tailwindcss/recommended',
+    'prettier'
+  ],
+  // Agrega cualquier configuración adicional según sea necesario
+  rules: {
+    // Define reglas adicionales si es necesario
+  },
+  // Agrega ajustes adicionales de ESLint según sea necesario
+};
